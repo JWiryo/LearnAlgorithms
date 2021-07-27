fcb8565446b8e9f5c3a3a5c28b24d32477c85658
James Wiryo <james.wiryo@gmail.com>
2021-07-27 15:02:50 +0000

    Implemented BubbleSort

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
+#Time Complexity = O(n^2)
+#Space Complexity = O(1)
 def bubbleSort(arr):
-  
+
   length = len(arr)
   for i in range(length-1):
     for j in range(length-1):
