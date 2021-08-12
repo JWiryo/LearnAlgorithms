72a76edefab1955b1811d78826591aba170639a4
James Wiryo <james.wiryo@gmail.com>
2021-08-12 11:43:43 +0000

    Implemented DFS pseudocode

--- TreeTraversal.py
+++ TreeTraversal.py
@@ -21,4 +21,54 @@ def BFS(root):
       queue.append(currentNode.right)
   
   return result
+
+# Pseudocode of In-Order DFS non-working implementation
+def DFSInOrder(root):
+
+  return traverseInOrder(root, [])
     
+def traverseInOrder(node, resultList):
+
+  if node.left:
+    traverseInOrder(node.left, resultList)
+  
+  resultList.append(node.value)
+
+  if node.right:
+    traverseInOrder(node.right, resultList)
+  
+  return resultList
+
+# Pseudocode of Pre-Order DFS non-working implementation
+def DFSPreOrder(root):
+
+  return traversePreOrder(root, [])
+    
+def traversePreOrder(node, resultList):
+
+  resultList.append(node.value)
+
+  if node.left:
+    traversePreOrder(node.left, resultList)
+
+  if node.right:
+    traversePreOrder(node.right, resultList)
+  
+  return resultList
+
+# Pseudocode of Post-Order DFS non-working implementation
+def DFSPostOrder(root):
+
+  return traversePostOrder(root, [])
+    
+def traversePostOrder(node, resultList):
+
+  if node.left:
+    traversePostOrder(node.left, resultList)
+
+  if node.right:
+    traversePostOrder(node.right, resultList)
+  
+  resultList.append(node.value)
+  
+  return resultList
